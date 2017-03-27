1c943eeaa37795e1f4696751b6eaafa452d12a2e
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2017-03-27 10:34:31 +0800

    moved to archive
    Signed-off-by: Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>

---
+++ archive/codelabs/includes/scripts/config.js
@@ -0,0 +1,7 @@
+var config = {
+   apiKey: "AIzaSyCeYsuRNmqoAtDa3k8oDohy8hflzCsc_IY",
+   authDomain: "gdg-devfest-philippines-2016.firebaseapp.com",
+   databaseURL: "https://gdg-devfest-philippines-2016.firebaseio.com",
+   storageBucket: "",
+ };
+ firebase.initializeApp(config);
--- codelabs/includes/scripts/config.js
+++
@@ -1,7 +0,0 @@
-var config = {
-   apiKey: "AIzaSyCeYsuRNmqoAtDa3k8oDohy8hflzCsc_IY",
-   authDomain: "gdg-devfest-philippines-2016.firebaseapp.com",
-   databaseURL: "https://gdg-devfest-philippines-2016.firebaseio.com",
-   storageBucket: "",
- };
- firebase.initializeApp(config);
